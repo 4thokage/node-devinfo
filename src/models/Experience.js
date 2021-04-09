6aaa75c7675bdce54da712fc05742ca22702c3ea
jrodrigues <jsr@jose-rodrigues.info>
2021-04-09 16:22:44 +0100

    Update experience model

--- src/models/Experience.js
+++ src/models/Experience.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose')
 
 const experienceSchema = new mongoose.Schema({
   name: String,
-  url: String
+  companyOrProjectURL: String,
+  startDate: String,
+  endDate: String,
+  responsibilities: Array
 })
 
 module.exports = mongoose.model('Experience', experienceSchema, 'experience')
